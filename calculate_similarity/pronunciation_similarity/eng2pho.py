eece251afa1e59514e7e1037e1b5a6ed42e931a1
white0098 <white0098@gmail.com>
2023-12-10 11:06:57 +0900

    커밋

---
+++ calculate_similarity/pronunciation_similarity/eng2pho.py
@@ -0,0 +1,29 @@
+import phonetics
+import eng_to_ipa as ipa
+from hangul_romanize import Transliter
+from hangul_romanize.rule import academic
+
+def convert_to_ipa(word):
+    return ipa.convert(word)
+
+def korean_to_english_pronunciation(korean_word):
+    transliter = Transliter(academic)
+    romanized = transliter.translit(korean_word)
+    return romanized
+
+
+word = "sam sung"
+korean_word = "삼 성"
+english_pronunciation = repr(korean_to_english_pronunciation(korean_word))
+
+
+ipa_representation = convert_to_ipa(word)
+ipa_representation_kor = convert_to_ipa(english_pronunciation)
+
+print(f"'{word}의 발음기호는 : {ipa_representation} 입니다.")
+
+print(f"'{korean_word}' 의 영어발음은 : {english_pronunciation} 입니다.")
+print(f"'{korean_word}' 의 발음기호는 : {ipa_representation_kor} 입니다.")
+
+
+
